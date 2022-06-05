672e3b1c41a7f428df67e618897e47f72e977b72
Viacheslav Chernogorov <slavachernogorv@mail.ru>
2022-06-05 17:57:50 +0300

    fix: fix script, header, main and another Part 1

--- script/script.js
+++ script/script.js
@@ -1,27 +1,29 @@
-let popup_open = document.querySelector(".profile-info__button");
+let popup_open = document.querySelector(".profile__button");
 let popup_close = document.querySelector(".popup__close-icon");
 let popup = document.querySelector(".popup");
 
 let formElement = document.querySelector(".popup__form");
-let nameInput = document.querySelector(".popup__name-input"); //
-let jobInput = document.querySelector(".popup__job-input"); // Назначаю переменную и вибираю откуда
+let nameInput = document.querySelector(".popup__field_type_name"); //
+let jobInput = document.querySelector(".popup__field_type_job"); // Назначаю переменную и вибираю откуда
 
-let Name = document.querySelector(".profile-info__title"); // Назначаю переменную Name и job
-let job = document.querySelector(".profile-info__subtitle");
-nameInput.value = Name.textContent; // Говорю, что значение nameInput and jobInput = тому, что вптсано в title and subtitle
-jobInput.value = job.textContent;
+let Name = document.querySelector(".profile__title"); // Назначаю переменную Name и job
+let job = document.querySelector(".profile__subtitle");
 
 popup_open.addEventListener("click", function (event) {
+  nameInput.value = Name.textContent; // Говорю, что значение nameInput and jobInput = тому, что вптсано в title and subtitle
+  jobInput.value = job.textContent;
+
   // Создаю функцию, которая добавляет модификатор и включает popup
-  event.preventDefault();
   popup.classList.add("popup_opened");
 });
 
+function myFunction() {
+  popup.classList.remove("popup_opened");
+}
+
 popup_close.addEventListener("click", function (event) {
   // Создаю функцию, которая убирает модификатор, закрывает popup и возвращает значения в формах к исходному значению
-  popup.classList.remove("popup_opened");
-  nameInput.value = Name.textContent;
-  jobInput.value = job.textContent;
+  myFunction();
 });
 
 function formSubmitHandler(evt) {
@@ -30,7 +32,7 @@ function formSubmitHandler(evt) {
   Name.textContent = nameInput.value;
   job.textContent = jobInput.value;
   // Выберите элементы, куда должны быть вставлены значения полей
-  popup.classList.remove("popup_opened");
+  myFunction();
   // Вставьте новые значения с помощью textContent
 }
 // Прикрепляем обработчик к форме:
